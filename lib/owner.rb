645a1c57e8fd95d2f783ce7aa97dbe09b11b199d
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-21 16:43:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,37 +27,37 @@ class Owner
   end
 
   def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
+    pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
-    @pets[:cats] << Cat.new(name)
+    pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    @pets[:dogs] << Dog.new(name)
+    pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
-    @pets[:dogs].each do |dog|
+    pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    @pets[:cats].each do |cat|
+    pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @pets[:fishes].each do |fish|
+    pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
 
   def sell_pets
-    @pets.each do |species, animals|
+    pets.each do |species, animals|
       animals.each do |animal|
         animal.mood = "nervous"
       end
