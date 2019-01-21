3945eb67cec5f8e759beced91539126cf51bdb04
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-21 16:45:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -69,7 +69,7 @@ class Owner
     fish = pets[:fishes].count
     dogs = pets[:dogs].count
     cats = pets[:cats].count
-    puts "I have #{fish} fish, #{dogs} dog(s), and #{cats} cat(s)."
+    "I have #{fish} fish, #{dogs} dog(s), and #{cats} cat(s)."
     end
   end
 
