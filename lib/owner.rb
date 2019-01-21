485d844606095079ecffba3eac7d0cde732efcb1
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-21 16:42:42 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,11 +57,12 @@ class Owner
   end
 
   def sell_pets
-    @pets.each do |pet|
-      pet.mood = "nervous"
+    @pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end 
+      animals.clear
     end
-
-    @pets.clear
   end
 
   def list_pets
